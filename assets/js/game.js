e68378f16f7016bacd1bed59380cbbf4751c9899
Darrik Crawford <darrikcrawford@yahoo.com>
2021-07-22 00:07:54 -0500

    null/blank name bug fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -85,8 +85,6 @@ var startGame = function() {
             // reset enemy.health before starting new fight
             pickedEnemyObj.health = randomNumber(40, 60);
   
-            // use debugger to pause script from running and check what's going on at that moment in the code
-            // debugger;
             
             // pass the pickedenemy.name variable's value into the fight function, where it will assume the value of the enemy.name parameter
             fight(pickedEnemyObj);
@@ -162,9 +160,21 @@ var shop = function() {
     }
 }
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 // player information
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
